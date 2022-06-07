d0be9faf69c6e499a23ea4130f6305b4e427d3e2
NehaThapa17 <er.nehathapa@gmail.com>
2022-06-07 07:37:12 +0000

    7June Commit

--- webapp/controller/CreateView.controller.js
+++ webapp/controller/CreateView.controller.js
@@ -6,12 +6,13 @@ sap.ui.define([
     "sap/ui/model/json/JSONModel",
     'sap/ui/core/Fragment',
     "sap/ui/model/Filter",
-    "sap/ui/model/FilterOperator"
+    "sap/ui/model/FilterOperator",
+    "sap/m/SearchField"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (UIComponent, History, Controller, JSONModel, Fragment, Filter, FilterOperator) {
+    function (UIComponent, History, Controller, JSONModel, Fragment, Filter, FilterOperator, SearchField) {
         "use strict";
 
         return Controller.extend("yamahamusic.so.createso.project.controller.CreateView", {
@@ -26,32 +27,44 @@ sap.ui.define([
                         {
                             "mat": "AA768",
                             "description": "Material For Test1",
-                            "sloc":"LOC1",
+                            "sloc": "LOC1",
                             "value": "20"
 
                         },
                         {
                             "mat": "AA564",
                             "description": "Material for Test2",
-                            "sloc":"LOC2",
+                            "sloc": "LOC2",
                             "value": "60"
 
                         },
                         {
                             "mat": "AA908",
                             "description": "Material for Test3",
-                            "sloc":"LOC3",
+                            "sloc": "LOC3",
                             "value": "80"
 
                         }
-                    ]
+                    ],
+                    "soldTP": [{
+                        "buyerID": "TBS010002",
+                        "buyerName": "The Brass Specialist",
+                    },
+                    {
+                        "buyerID": "TBS010003",
+                        "buyerName": "The Brass Buyer",
+                    },
+                    {
+                        "buyerID": "TBS010004",
+                        "buyerName": "Buyer C",
+                    }]
                 };
                 // var oMatData = {};
 
                 oModel.setData(oItemData);
                 this.getView().setModel(oModel, "itemModel");
                 this.addItem();
-                
+
                 //*******Calendar Default Date to 3days from current date *******//
                 var dateCurr = new Date();
                 dateCurr.setDate(dateCurr.getDate() + 3);
@@ -66,7 +79,7 @@ sap.ui.define([
                 // this.getView().byId(dynId).setMinDate(new Date());
                 // this.getView().byId(dynId).setMaxDate(new Date(date));
                 //*******Calendar Limit to 40days *******//
-                
+
 
             },
 
@@ -195,13 +208,22 @@ sap.ui.define([
                 this._pValueHelpDialog.then(function (oDialog) {
                     // Create a filter for the binding
                     oDialog.getBinding("items").filter([new Filter("mat", FilterOperator.Contains, sInputValue)]);
+                    oDialog.getBinding("items").filter([new Filter("description", FilterOperator.Contains, sInputValue)]);
                     // Open ValueHelpDialog filtered by the input's value
-                    oDialog.open(sInputValue);
+                    oDialog.open();
                 });
             },
             onValueHelpSearch: function (oEvent) {
                 var sValue = oEvent.getParameter("value");
-                var oFilter = new Filter("mat", FilterOperator.Contains, sValue);
+
+                // var oFilter = new Filter("mat", FilterOperator.Contains, sValue);
+                var oFilter = new Filter({
+                    filters: [
+                        new Filter("mat", FilterOperator.Contains, sValue),
+                        new Filter("description", FilterOperator.Contains, sValue)
+                    ],
+                    and: false
+                });
 
                 oEvent.getSource().getBinding("items").filter([oFilter]);
             },
@@ -215,7 +237,7 @@ sap.ui.define([
                 if (!oSelectedItem) {
                     return;
                 }
-                var oIdDsc = this.getView().getId() + "--idMatDscTxt-" + this.getView().getId() + "--idTableItems-" + oId.charAt(oId.length - 1) ;
+                var oIdDsc = this.getView().getId() + "--idMatDscTxt-" + this.getView().getId() + "--idTableItems-" + oId.charAt(oId.length - 1);
                 this.getView().byId(oId).setValue(selMat[0]);
                 this.getView().byId(oIdDsc).setText(selMatDsc[0]);
             },
@@ -233,42 +255,88 @@ sap.ui.define([
             //     else {
             //         this.getView().byId("idDPHeader").setEnabled(false);
             //     }
-                
-            // },
-            onBuyerPartyF4Help: function(oEvent){
-                var sInputValue = oEvent.getSource().getValue(),
-                oView = this.getView();
-                if (!this._pValueHelpDialogSH) {
-                    this._pValueHelpDialogSH = Fragment.load({
-                        id: oView.getId(),
-                        name: "yamahamusic.so.createso.project.fragments.BuyerPartyList",
-                        controller: this
-                    }).then(function (oDialog) {
-                        oView.addDependent(oDialog);
-                        return oDialog;
-                    });
-                }
-                this._pValueHelpDialogSH.then(function (oDialog) {
-                    // Create a filter for the binding
-                    // oDialog.getBinding("items").filter([new Filter("mat", FilterOperator.Contains, sInputValue)]);
-                    // Open ValueHelpDialog filtered by the input's value
-                    oDialog.open();
-                });
-                
+
+            // }, 
+            onBuyerPartyF4Help: function (oEvent) {
+                this._oInputField = oEvent.getSource();
+                var sFragmentName = "yamahamusic.so.createso.project.fragments.BuyerPartyList";
+                var oModelData = this.getView().getModel("itemModel");
+                var oModelColumns = new JSONModel();
+                var oCol = {
+                    cols: [{
+                        "label": "Buyer ID",
+                        "template": "buyerID"
+                    },
+                    {
+                        "label": "Buyer Name",
+                        "template": "buyerName"
+                    }]
+                };
+                oModelColumns.setData(oCol);
+
+                this.openCommonDialogVH(sFragmentName, oModelData, oModelColumns);
+
             },
-            onValueHelpCloseSH:function(oEvent){
+            openCommonDialogVH: function (sFragmentName, oModelData, oModelColumns) {
+                // @ts-ignore
+                this._oBasicSearchField = new SearchField({
+                    showSearchButton: false
+                });
+
+                var aCols = oModelColumns.getData().cols;
+
+                Fragment.load({
+                    name: sFragmentName,
+                    controller: this
+                }).then(function name(oFragment) {
+                    this._oValueHelpDialog = oFragment;
+                    this.getView().addDependent(this._oValueHelpDialog);
+
+                    //set filter bar in dialog
+                    var oFilterBar = this._oValueHelpDialog.getFilterBar();
+                    oFilterBar.setFilterBarExpanded(false);
+                    oFilterBar.setBasicSearch(this._oBasicSearchField);
+
+                    //set table in dialog
+                    this._oValueHelpDialog.getTableAsync().then(function (oTable) {
+                        oTable.setModel(oModelData);
+                        oTable.setModel(oModelColumns, "columns");
+
+                        if (oTable.bindRows) {
+                            oTable.bindAggregation("rows", "/soldTP");
+                        }
+
+                        if (oTable.bindItems) {
+                            oTable.bindAggregation("items", "/soldTP", function () {
+                                return new ColumnListItem({
+                                    cells: aCols.map(function (column) {
+                                        return new Label({ text: "{" + column.template + "}" });
+                                    })
+                                });
+                            });
+                        }
+                        var oSorter = new sap.ui.model.Sorter(
+                            aCols[0].template
+                        );
+                        oTable.getBinding("rows").sort(oSorter);
+                        this._oValueHelpDialog.update();
+                    }.bind(this));
+
+                    this._oValueHelpDialog.open();
+                }.bind(this));
+            },
+            onValueHelpCloseSH: function (oEvent) {
                 var oSelectedItem = oEvent.getParameter("selectedItem");
-                
+
                 var aContexts = oEvent.getParameter("selectedContexts");
-               
+
                 if (!oSelectedItem) {
                     return;
-                } else
-               {
+                } else {
                     this.getView().byId("idBuyerIdInput").setValue(oSelectedItem.getTitle());
                     this.getView().byId("idBuyerIdInput").setDescription(oSelectedItem.getDescription());
                     this.getView().byId("idShipToInput").setEnabled(true);
-               }
+                }
             },
             onChangeCRDate: function (oEvt) {
                 debugger;
@@ -315,6 +383,82 @@ sap.ui.define([
                 oDP.setDateValue(oSelDate);
 
             },
+            onValueHelpOkPress: function (oEvent) {
+                var aTokens = oEvent.getParameter("tokens");
+                var sValue = aTokens[0].getKey(),
+                    sDesc = aTokens[0].getText().split("("),
+                    sTxt = sDesc[0];
+
+                // @ts-ignore
+                this._oValueHelpDialog.close();
+
+                // @ts-ignore
+                if (this._oInputField) {
+                    // @ts-ignore
+                    this._oInputField.setValue(sValue);
+                    this._oInputField.setDescription(sTxt);
+                    this.getView().byId("idShipToInput").setEnabled(true);
+                }
+            },
+            onValueHelpCancelPress: function () {
+                // @ts-ignore
+                this._oValueHelpDialog.close();
+            },
+            onValueHelpAfterClose: function () {
+                // @ts-ignore
+                this._oInputField = "";
+                // @ts-ignore
+                this._oValueHelpDialog.destroy();
+            },
+
+            commonFilterBarSearch: function (oEvent) {
+                var aSelectionSet = oEvent.getParameter("selectionSet");
+                // @ts-ignore
+                var sSearchQuery = this._oBasicSearchField.getValue();
+                var aFilters = aSelectionSet.reduce(function (aResult, oControl) {
+                    if (oControl.getValue()) {
+                        aResult.push(new Filter({
+                            path: oControl.getName(),
+                            operator: FilterOperator.Contains,
+                            value1: oControl.getValue()
+                        }));
+                    }
+
+                    return aResult;
+                }, []);
+
+                //set OR filters
+                var aFilterCombo = [];
+                for (var i = 0; i < aSelectionSet.length; i++) {
+                    aFilterCombo.push(new Filter({ path: aSelectionSet[i].getName(), operator: FilterOperator.Contains, value1: sSearchQuery }))
+                }
+                aFilters.push(new Filter({
+                    filters: aFilterCombo,
+                    and: false
+                }));
+
+                //call all filters
+                this._filterTable(new Filter({
+                    filters: aFilters,
+                    and: true
+                }));
+            },
+            _filterTable: function (oFilter) {
+                // @ts-ignore
+                var oValueHelpDialog = this._oValueHelpDialog;
+
+                oValueHelpDialog.getTableAsync().then(function (oTable) {
+                    if (oTable.bindRows) {
+                        oTable.getBinding("rows").filter(oFilter);
+                    }
+
+                    if (oTable.bindItems) {
+                        oTable.getBinding("items").filter(oFilter);
+                    }
+
+                    oValueHelpDialog.update();
+                });
+            }
 
         });
     });
